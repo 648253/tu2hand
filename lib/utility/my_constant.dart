744597937697e8983102d7faf709e4d82c96518f
Suhai <suhai.p@sci.tu.ac.th>
2021-08-20 02:03:37 +0700

    Start App

---
+++ lib/utility/my_constant.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Myconstant {
+  // General
+  static String appname = 'TU2HAND';
+
+  // Route
+  static String routeAuthen = '/authen';
+  static String routeCreateAccount = '/createAccount';
+  static String routeBuyerService = '/buyerService';
+  static String routeSellerService = '/sellerService';
+}
